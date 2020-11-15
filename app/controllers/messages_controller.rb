a2391787e1af50b3a8ca1937a64c2b6ca3e05499
Douglas Berkley <douglasmberkley@gmail.com>
2020-11-15 20:05:28 +0900

    fixed views

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -10,4 +10,10 @@ class MessagesController < ApplicationController
       render "chatrooms/show"
     end
   end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content)
+  end
 end
